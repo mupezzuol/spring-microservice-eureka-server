51011534ba113a5da920af3a9817eb894ad62dcd
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-01-06 19:08:42 -0300

    Add eureka server configs default

--- src/main/java/br/com/microservice/eureka/SpringMicroserviceEurekaServerApplication.java
+++ src/main/java/br/com/microservice/eureka/SpringMicroserviceEurekaServerApplication.java
@@ -2,8 +2,10 @@ package br.com.microservice.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringMicroserviceEurekaServerApplication {
 
 	public static void main(String[] args) {

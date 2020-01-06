90b6e9c61aca519aa3b9b57b79c8a9dec5139109
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-01-06 18:46:13 -0300

    Upload Project

---
+++ src/main/java/br/com/microservice/eureka/SpringMicroserviceEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservice.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/microservice/eureka/SpringMicroserviceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservice.eureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
